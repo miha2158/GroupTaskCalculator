36aadb224bedfc3e6feb6296a142712f1d3aae4c
miha2158 <miha.vanya@gmail.com>
2017-06-07 11:45:46 +0500

    interface done #1

--- GroupTaskCalculator/Form1.Designer.cs
+++ GroupTaskCalculator/Form1.Designer.cs
@@ -1,16 +1,11 @@
-﻿namespace GroupTaskCalculator
+﻿using System.Windows.Forms;
+
+namespace GroupTaskCalculator
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -19,21 +14,145 @@
             }
             base.Dispose(disposing);
         }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+        
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.InitialNS = new System.Windows.Forms.ComboBox();
+            this.DestinationNS = new System.Windows.Forms.ComboBox();
+            this.InitialNumber = new System.Windows.Forms.TextBox();
+            this.DestinationNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DoAction = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InitialNS
+            // 
+            this.InitialNS.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.InitialNS.FormattingEnabled = true;
+            this.InitialNS.Location = new System.Drawing.Point(14, 38);
+            this.InitialNS.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.InitialNS.Name = "InitialNS";
+            this.InitialNS.Size = new System.Drawing.Size(140, 25);
+            this.InitialNS.TabIndex = 0;
+            this.InitialNS.SelectedIndexChanged += new System.EventHandler(this.InitialNS_SelectedIndexChanged);
+            // 
+            // DestinationNS
+            // 
+            this.DestinationNS.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.DestinationNS.FormattingEnabled = true;
+            this.DestinationNS.Location = new System.Drawing.Point(14, 116);
+            this.DestinationNS.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.DestinationNS.Name = "DestinationNS";
+            this.DestinationNS.Size = new System.Drawing.Size(140, 25);
+            this.DestinationNS.TabIndex = 1;
+            this.DestinationNS.SelectedIndexChanged += new System.EventHandler(this.DestinationNS_SelectedIndexChanged);
+            // 
+            // InitialNumber
+            // 
+            this.InitialNumber.Location = new System.Drawing.Point(167, 38);
+            this.InitialNumber.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.InitialNumber.Multiline = true;
+            this.InitialNumber.Name = "InitialNumber";
+            this.InitialNumber.ReadOnly = true;
+            this.InitialNumber.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.InitialNumber.Size = new System.Drawing.Size(389, 124);
+            this.InitialNumber.TabIndex = 2;
+            this.InitialNumber.TextChanged += new System.EventHandler(this.InitialNumber_TextChanged);
+            // 
+            // DestinationNumber
+            // 
+            this.DestinationNumber.Location = new System.Drawing.Point(17, 211);
+            this.DestinationNumber.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.DestinationNumber.Multiline = true;
+            this.DestinationNumber.Name = "DestinationNumber";
+            this.DestinationNumber.ReadOnly = true;
+            this.DestinationNumber.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.DestinationNumber.Size = new System.Drawing.Size(539, 127);
+            this.DestinationNumber.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Из";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Системы счисления";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Систему счисления";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "В";
+            // 
+            // DoAction
+            // 
+            this.DoAction.Enabled = false;
+            this.DoAction.Location = new System.Drawing.Point(17, 166);
+            this.DoAction.Name = "DoAction";
+            this.DoAction.Size = new System.Drawing.Size(539, 38);
+            this.DoAction.TabIndex = 8;
+            this.DoAction.Text = "Перевести";
+            this.DoAction.UseVisualStyleBackColor = true;
+            this.DoAction.Click += new System.EventHandler(this.DoAction_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(568, 351);
+            this.Controls.Add(this.DoAction);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DestinationNumber);
+            this.Controls.Add(this.InitialNumber);
+            this.Controls.Add(this.DestinationNS);
+            this.Controls.Add(this.InitialNS);
+            this.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
-        #endregion
+        private System.Windows.Forms.ComboBox InitialNS;
+        private System.Windows.Forms.ComboBox DestinationNS;
+        private System.Windows.Forms.TextBox InitialNumber;
+        private System.Windows.Forms.TextBox DestinationNumber;
+        private System.Windows.Forms.Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button DoAction;
     }
 }
 
--- GroupTaskCalculator/Form1.cs
+++ GroupTaskCalculator/Form1.cs
@@ -15,6 +15,100 @@ namespace GroupTaskCalculator
         public Form1()
         {
             InitializeComponent();
+            InitialNS.Items.AddRange(PossibleNS);
+            DestinationNS.Items.AddRange(PossibleNS);
+            InitialNumber.KeyPress += InitialNumber_KeyPress;
+        }
+
+        private static readonly object[] PossibleNS = { 2, 3, 8, 9, 10, 16 };
+
+        private void InitialNumber_TextChanged(object sender, EventArgs e)
+        {
+            /*string temp = InitialNumber.Text;
+
+            InitialNumber.Text = temp
+                .Replace("- ", "-")
+                .Replace("--", "-")
+                .Replace(",-", ",")
+                .Replace(",,", ",")
+                .Replace("-,", "-");*/
+            try
+            {
+                InitialNumber_KeyPress(sender, (KeyPressEventArgs)e);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void InitialNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (InitialNS.SelectedItem)
+            {
+                case 2:
+                {
+                    if (!(new[] { '0', '1', '-', ',', ' ' }.Contains(e.KeyChar) || char.IsControl(e.KeyChar)))
+                        e.Handled = true;
+                }
+                break;
+
+                case 3:
+                {
+                    if (!(new[] { '0', '1', '2', '-', ',',' ' }.Contains(e.KeyChar) || char.IsControl(e.KeyChar)))
+                        e.Handled = true;
+                }
+                break;
+
+                case 8:
+                {
+                    if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar) || new[] { '-', ',', ' ' }.Contains(e.KeyChar)))
+                        e.Handled = true;
+                    else
+                        if (new[] { '8', '9' }.Contains(e.KeyChar))
+                            e.Handled = true;
+                }
+                break;
+
+                case 9:
+                {
+                    if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar) || new[] { '-', ',', ' ' }.Contains(e.KeyChar)))
+                        e.Handled = true;
+                    else
+                        if (e.KeyChar == '9')
+                            e.Handled = true;
+                }
+                break;
+
+                case 10:
+                {
+                    if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar) || new[] { '-', ',', ' ' }.Contains(e.KeyChar)))
+                        e.Handled = true;
+                }
+                break;
+
+                case 16:
+                {
+                    if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar) || new[] { '-', ' ', ',','a','b','c','d','e','f' }.Contains(e.KeyChar.ToString().ToLower()[0])))
+                        e.Handled = true;
+                }
+                break;
+            }
+        }
+
+        private void InitialNS_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            InitialNumber.ReadOnly = false;
+        }
+
+        private void DoAction_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DestinationNS_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (InitialNS.SelectedIndex != -1)
+                DoAction.Enabled = true;
         }
     }
 }
