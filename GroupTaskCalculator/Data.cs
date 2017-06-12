9ad3af1cf04e9b541bbe00a6b5997edc071c856f
Kompoman32 <Kompoman32@yandex.ru>
2017-06-12 20:27:09 +0500

    +comments

--- GroupTaskCalculator/Data.cs
+++ GroupTaskCalculator/Data.cs
@@ -4,7 +4,7 @@ namespace GroupTaskCalculator
 {
     public class Data
     {
-        private static string alph = "0123456789ABCDEF";
+        private static string alph = "0123456789ABCDEF"; // Возможный алфавит
         private readonly string value;
         public string Value => value;
         private readonly int numSystem;
@@ -14,26 +14,28 @@ namespace GroupTaskCalculator
         {
             this.value = value;
             this.numSystem = numSystem;
-        }
+        }// Конструктор
 
         public static Data Convert(Data inputValue, int outputCC)
         {
-            if (inputValue.numSystem == outputCC) return inputValue;
-            string sign= inputValue.Value[0] == '-'?"-":"";
+            if (inputValue.numSystem == outputCC) return inputValue; // Если системы счисления равны, зачем идти дальше?
+
+            string sign= inputValue.Value[0] == '-'?"-":""; // Запомнить знак числа
             string firstPart = "";
             string secondPart = "";
-            if (inputValue.value.IndexOf(",") > 0)
+            if (inputValue.value.IndexOf(",") > 0) // Разбить число на целую и дробную часть
             {
                 firstPart = inputValue.value.Remove(inputValue.value.IndexOf(","));
                 secondPart = inputValue.value.Substring(inputValue.value.IndexOf(",") + 1);
             }
             else firstPart = inputValue.value;
             firstPart = firstPart.Trim('-');
+
             decimal outputValue=0;
 
             #region Перевод в десятичную
 
-            if (inputValue.numSystem != 10)
+            if (inputValue.numSystem != 10) // Если начальная система счисления 10, то перевод не требуется
             {
                 for (int i = 0; i < firstPart.Length; i++)
                 {
@@ -54,8 +56,9 @@ namespace GroupTaskCalculator
             #region Перевод в target систему
 
             string outputString;
-            if (outputCC != 10)
+            if (outputCC != 10) // Если нужная система счисления 10, то перевод не требуется
             {
+                // Целая часть
                 ulong intPart = (ulong) outputValue;
                 decimal doublePart = outputValue - intPart;
 
@@ -69,7 +72,7 @@ namespace GroupTaskCalculator
                 outputStringIntPart = intPart + outputStringIntPart;
                 outputStringIntPart = outputStringIntPart.TrimStart('0');
 
-
+                //Дробная часть
                 string outputStringdoublePart = "";
                 while (doublePart != 0.0m && outputStringdoublePart.Length < 10)
                 {
@@ -87,13 +90,14 @@ namespace GroupTaskCalculator
 
             #endregion Перевод в target систему
 
+            // Ограничение в 10 знаков после запятой
             if (outputString.Contains(",") && outputString.Length - outputString.IndexOf(',') - 1 > 10)
                 outputString=outputString.Remove(outputString.IndexOf(',') + 11);
 
             if (sign + outputString == "-0")
                 return new Data("0", outputCC);
             return new Data(sign+outputString, outputCC);
-        }
+        }// Перевод
     }
 }
 
